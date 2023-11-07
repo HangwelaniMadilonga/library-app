7888f851ca12aba2f5805958dfa877c3d8ef028f
Hangwelani Madilonga <hangwelani3@gmail.com>
2023-11-07 13:24:34 +0200

    Create layout and styling. Add form section.

---
+++ app.js
@@ -0,0 +1,21 @@
+// const myLibrary = [];
+// let isRunning = true;
+
+// function Book(title, author,totalPages,isRead) {
+//     this.title = title;
+//     this.author = author;
+//     this.totalPages = totalPages;
+//     this.isRead = false;
+//     this.info = function() {
+//       console.log('${title}')
+//     };
+//   }
+  
+// while(isRunning){
+//     let userInput = prompt("Enter an item or press enter to finish:");
+// }
+
+//   const player1 = new Player('steve', 'X');
+//   const player2 = new Player('also steve', 'O');
+//   player1.sayName(); 
+//   player2.sayName(); 
