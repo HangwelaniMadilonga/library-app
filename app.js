17ddb0524b05d59d295d914c485d0e564ce3f5f3
Hangwelani Madilonga <hangwelani3@gmail.com>
2023-11-09 10:55:58 +0200

    Add a favicon for webpage.

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-// const myLibrary = [];
-// let isRunning = true;
+const myLibrary = [];
 
-// function Book(title, author,totalPages,isRead) {
-//     this.title = title;
-//     this.author = author;
-//     this.totalPages = totalPages;
-//     this.isRead = false;
-//     this.info = function() {
-//       console.log('${title}')
-//     };
-//   }
+function Book(title, author,totalPages,isRead) {
+    this.title = title;
+    this.author = author;
+    this.totalPages = totalPages;
+    this.isRead = false;
+    this.info = function() {
+      
+    };
+  }
   
-// while(isRunning){
-//     let userInput = prompt("Enter an item or press enter to finish:");
-// }
+while(isRunning){
+    let userInput = prompt("Enter an item or press enter to finish:");
+}
 
-//   const player1 = new Player('steve', 'X');
-//   const player2 = new Player('also steve', 'O');
-//   player1.sayName(); 
-//   player2.sayName(); 
+  const player1 = new Player('steve', 'X');
+  const player2 = new Player('also steve', 'O');
+  player1.sayName(); 
+  player2.sayName(); 
